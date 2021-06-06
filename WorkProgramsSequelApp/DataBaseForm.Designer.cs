c7358c92c24e372d87d9bb882bab61d72ab3c284
20erik03 20erik03 <margiev_e@mail.ru>
2021-06-07 00:21:04 +0300

    Забыл кита(

--- WorkProgramsSequelApp/DataBaseForm.Designer.cs
+++ WorkProgramsSequelApp/DataBaseForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataBaseForm));
             this.panel2 = new System.Windows.Forms.Panel();
             this.buttonSaveDB = new System.Windows.Forms.Button();
             this.buttonChangeDB = new System.Windows.Forms.Button();
@@ -152,6 +153,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DataBaseForm";
             this.Text = "Рабочие программы база даных";
